d55f5c8ca0eb2333fa3e8e37379f2366fd9ee9ce
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-05-12 22:15:20 -0500

    Update dependencies, make Account and Address more interesting

--- accounts.py
+++ accounts.py
@@ -1,9 +1,34 @@
 from nameko.rpc import rpc
+from nameko_redis import Redis
+import uuid
 
 
 class AccountService:
     name = 'account_service'
 
+    redis = Redis('development')
+
+    def _schema(self, **kwargs):
+        return {
+            'email_address': kwargs.get('email_address', None),
+            'first_name': kwargs.get('first_name', None),
+            'last_name': kwargs.get('last_name', None),
+            'billing_address_id': kwargs.get('billing_address_id', None),
+            'shipping_address_id': kwargs.get('shipping_address_id', None),
+        }
+
     @rpc
-    def get_account(self, account_id):
-        return { 'account_id': account_id }
+    def create(self, email_address):
+        assert(email_address)
+
+        data = self._schema(email_address=email_address)
+
+        account_id = uuid.uuid4().hex
+        self.redis.set(account_id, data)
+
+        return account_id
+
+    @rpc
+    def get(self, account_id):
+        account = self.redis.get(account_id)
+        return account
--- addresses.py
+++ addresses.py
@@ -1,13 +1,40 @@
 from nameko.rpc import rpc
+from nameko_redis import Redis
 
+import uuid
 
 class AddressService:
     name = 'address_service'
 
+    redis = Redis('development')
+
+    def _schema(self, **kwargs):
+        return {
+            'street_1': kwargs.get('street_1', None),
+            'street_2': kwargs.get('street_2', None),
+            'city': kwargs.get('city', None),
+            'province': kwargs.get('province', None),
+            'postal_code': kwargs.get('postal_code', None),
+            'country': kwargs.get('country', None),
+        }
+
     @rpc
     def create(self, street_1, street_2, city, province, postal_code, country):
-        return
+        address_id = uuid.uuid4().hex
+
+        data = self._schema(
+            street_1=street_1,
+            street_2=street_2,
+            city=city,
+            province=province,
+            postal_code=postal_code,
+            country=country,
+        )
+
+        self.redis.set(address_id, data)
+        return address_id
 
     @rpc
     def get(self, address_id):
-        return { 'address_id': address_id }
+        address = self.redis.get(address_id)
+        return address
