0a7afa6588e68b71880c246643562b9bb570fcdf
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-05-16 14:35:29 -0500

    Some changes to the listing service

--- listings/service.py
+++ listings/service.py
@@ -10,26 +10,24 @@ class ListingService:
     redis = Redis('development')
 
     def _schema(self, **kwargs):
-        return {
+        _data = {
+            'id': kwargs.get('id', uuid.uuid4().hex),
             'seller_account_id': kwargs.get('seller_account_id', None),
             'product_name': kwargs.get('product_name', None),
             'quantity': kwargs.get('quantity', None),
+            'price': kwargs.get('price', None),
         }
 
+        return dict((k, v) for k, v in _data.items() if v is not None)
+
     @rpc
-    def create(self, seller_account_id, product_name, quantity):
-        data = self._schema(
-            seller_account_id=seller_account_id,
-            product_name=product_name,
-            quantity=quantity,
-        )
-
-        listing_id = uuid.uuid4().hex
-        self.redis.set(listing_id, data)
-
+    def create(self, **kwargs):
+        data = self._schema(**kwargs)
+        listing_id = data.get('id')
+        self.redis.hmset(listing_id, data)
         return listing_id
 
     @rpc
     def get(self, listing_id):
-        listing = self.redis.get(listing_id)
+        listing = self.redis.hgetall(listing_id)
         return listing
