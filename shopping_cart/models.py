6b97e92d29d7c320b51c0f5f924304c97103f14a
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-05-22 17:03:50 -0500

    Flesh out the model, use a dict for internal storage

--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -2,30 +2,43 @@ from typing import Dict
 
 
 class ShoppingCart:
-    def __init__(self, account_id, listings=None):
+    def __init__(self, account_id, listing_id = None, quantity = None, listings: Dict = None):
         assert(account_id)
         self._account_id = account_id
+        self._listings = dict()
 
-        if listings is None:
-            self._listings = set()
-        else:
-            self._listings = set(listings)
+        if listing_id is not None:
+            assert(quantity)
+            self._listings[listing_id] = int(quantity)
 
-    def add_listing(self, listing_id):
-        self._listings.add(listing_id)
+        if listings is not None:
+            self._listings.update(listings)
 
-    def remove_listing(self, listing_id):
-        self._listings.remove(listing_id)
+    def add_listing(self, listing_id, quantity) -> int:
+        assert(quantity > 0)
+        current_quantity = self._listings.get(listing_id, 0)
+        self._listings[listing_id] = current_quantity + int(quantity)
+        return self._listings.get(listing_id)
+
+    def remove_listing(self, listing_id, quantity=None) -> int:
+        new_quantity = 0
+        if quantity is not None:
+            assert(quantity > 0)
+            current_quantity = self._listings.get(listing_id, 0)
+            new_quantity = current_quantity - int(quantity)
+            if new_quantity <= 0:
+                self._listings.remove(listing_id)
+        return new_quantity
 
     def to_dict(self):
         return {
-            'account_id' : self._account_id,
-            'listings' : list(self._listings),
+            'account_id': self._account_id,
+            'listings': self._listings,
         }
 
     @classmethod
-    def from_dict(cls, data_dict: Dict):
+    def from_dict(cls, data_dict: Dict) -> 'ShoppingCart':
         account_id = data_dict.get('account_id')
-        listings = data_dict.get('listings', [])
+        listings = data_dict.get('listings', {})
         return cls(account_id, listings=listings)
 
