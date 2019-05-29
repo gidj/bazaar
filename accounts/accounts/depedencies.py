fc2dff7f1fa2109026825539c986bf34012d4ff1
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-05-28 21:17:34 -0500

    Refactor to put storage in a dependency

---
+++ accounts/accounts/depedencies.py
@@ -0,0 +1,78 @@
+import logging
+import uuid
+from typing import AnyStr, Dict
+
+from nameko.extensions import DependencyProvider
+from redis import StrictRedis as _StrictRedis
+
+
+class NotFoundException(Exception):
+    pass
+
+
+REDIS_URIS_KEY = 'REDIS_URIS'
+
+
+class RedisWrapper:
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, redis_client):
+        self.redis = redis_client
+
+    def _generate_id(self) -> AnyStr:
+        return uuid.uuid4().hex
+
+    def _format_key(self, account_id) -> AnyStr:
+        return "accounts:{}".format(account_id)
+
+    def _schema(self, data_dict: Dict) -> Dict:
+        return {
+            'id': data_dict.get('id'),
+            'email_address': data_dict.get('email_address'),
+            'first_name': data_dict.get('first_name', None),
+            'last_name': data_dict.get('last_name', None),
+            'billing_address_id': data_dict.get('billing_address_id', None),
+            'shipping_address_id': data_dict.get('shipping_address_id', None),
+        }
+
+    def create(self, data: Dict) -> AnyStr:
+        assert (data.get('email_address', None))
+        _id = self._generate_id()
+        data['id'] = _id
+        self.redis.hmset(self._format_key(_id), data)
+        return _id
+
+    def get(self, account_id: AnyStr) -> Dict:
+        account = self.redis.hgetall(self._format_key(account_id))
+        if not account:
+            raise NotFoundException(
+                "Account ID {} does not exist".format(account_id))
+        else:
+            return self._schema(account)
+
+
+class Storage(DependencyProvider):
+    def __init__(self, key='development', **options):
+        self.key = key
+        self.client = None
+        self.options = {
+            'decode_responses': True,
+        }
+        self.options.update(options)
+
+    def setup(self):
+        redis_uris = self.container.config[REDIS_URIS_KEY]
+        self.redis_uri = redis_uris[self.key]
+
+    def start(self):
+        self.client = _StrictRedis.from_url(self.redis_uri, **self.options)
+
+    def stop(self):
+        self.client = None
+
+    def kill(self):
+        self.client = None
+
+    def get_dependency(self, worker_ctx):
+        wrapper = RedisWrapper(self.client)
+        return wrapper
--- accounts/accounts/service.py
+++ accounts/accounts/service.py
@@ -1,37 +1,23 @@
 import logging
-import uuid
+from typing import Dict
 
 from nameko.rpc import rpc
-from nameko_redis import Redis
+
+from accounts import depedencies
 
 
 class AccountService:
     logger = logging.getLogger(__name__)
     name = 'accounts'
 
-    redis = Redis('development')
-
-    def _schema(self, **kwargs):
-        _data = {
-            'id': kwargs.get('id', uuid.uuid4().hex),
-            'email_address': kwargs.get('email_address', None),
-            'first_name': kwargs.get('first_name', None),
-            'last_name': kwargs.get('last_name', None),
-            'billing_address_id': kwargs.get('billing_address_id', None),
-            'shipping_address_id': kwargs.get('shipping_address_id', None),
-        }
-
-        return dict((k, v) for k, v in _data.items() if v is not None)
+    storage = depedencies.Storage()
 
     @rpc
-    def create(self, **kwargs):
-        assert (kwargs.get('email_address', None))
-        data = self._schema(**kwargs)
-        self.redis.hmset(data.get('id'), data)
-        return data.get('id')
+    def create(self, data: Dict):
+        assert (data.get('email_address', None))
+        _id = self.storage.create(data)
+        return _id
 
     @rpc
     def get(self, account_id):
-        self.logger.info(account_id)
-        account = self.redis.hgetall(account_id)
-        return account
+        return self.storage.get(account_id)
