2555c4f95fd3bd938c8272c57453a8181f328f0d
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-05-29 23:14:28 -0500

    Break out data later from service layer

--- listings/listings/dependencies.py
+++ listings/listings/dependencies.py
@@ -55,3 +55,30 @@ class RedisWrapper:
         listing.update(updates)
         self._write(listing_id, listing)
         return listing
+
+
+class Storage(DependencyProvider):
+    def __init__(self, key='development', **options):
+        self.key = key
+        self.client = None
+        self.options = {
+            'decode_responses': True,
+        }
+        self.options.update(options)
+
+    def setup(self):
+        redis_uris = self.container.config[REDIS_URI_KEY]
+        self.redis_uri = redis_uris[self.key]
+
+    def start(self):
+        self.client = _StrictRedis.from_url(self.redis_uri, **self.options)
+
+    def stop(self):
+        self.client = None
+
+    def kill(self):
+        self.client = None
+
+    def get_dependency(self, worker_ctx):
+        wrapper = RedisWrapper(self.client)
+        return wrapper
--- listings/listings/service.py
+++ listings/listings/service.py
@@ -1,48 +1,28 @@
+import logging
 import uuid
+from typing import Dict
 
 from nameko.rpc import rpc
 from nameko_redis import Redis
 
-import logging
+from listings import dependencies
 
 
 class ListingService:
     logger = logging.getLogger(__name__)
-    name = 'listings'
+    name = "listings"
 
-    redis = Redis('development')
-
-    def _schema(self, **kwargs):
-        _data = {
-            'id': kwargs.get('id', uuid.uuid4().hex),
-            'seller_account_id': kwargs.get('seller_account_id', None),
-            'product_name': kwargs.get('product_name', None),
-            'quantity': kwargs.get('quantity', None),
-            'price': kwargs.get('price', None),
-        }
-
-        return dict((k, v) for k, v in _data.items() if v is not None)
+    storage = dependencies.Storage()
 
     @rpc
-    def create(self, **kwargs):
-        data = self._schema(**kwargs)
-        listing_id = data.get('id')
-        listing_id = self._write_data(listing_id, data)
-        return listing_id
-
-    @rpc
-    def update(self, listing_id, **kwargs):
-        listing = self.get(listing_id)
-        data = self._schema(id=listing_id, **kwargs)
-        listing.update(data)
-        self._write_data(listing_id, data)
+    def create(self, data: Dict):
+        _id = self.storage.create(data)
+        return _id
 
     @rpc
     def get(self, listing_id):
-        listing = self.redis.hgetall(listing_id)
-        self.logger.info(listing)
-        return listing
+        return self.storage.get(listing_id)
 
-    def _write_data(self, listing_id, data):
-        self.redis.hmset(listing_id, data)
-        return listing_id
+    @rpc
+    def update(self, listing_id, **kwargs):
+        return self.storage.update(listing_id)
